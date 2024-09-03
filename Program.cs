debe9dc24f2c5a5e6bfdc00d28dc82da574965f2
Noe <noe.flores@teccsup.edu.pe>
2024-09-02 20:09:25 -0500

    actualizado

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddCors(options =>
         });
 });
 
+builder.WebHost.UseUrls("http://*:8080");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- obj/Debug/net8.0/apisem.AssemblyInfo.cs
+++ obj/Debug/net8.0/apisem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apisem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+768791009089fe7abfa1b4a8ea285c4410791ede")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc28ad06d22fe61a440368f723d3a596a265b70c")]
 [assembly: System.Reflection.AssemblyProductAttribute("apisem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apisem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
